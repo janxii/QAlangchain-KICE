ebfeefdc6c5058964e5709cf48c93f7f4fb3524b
janxii <133779609+janxii@users.noreply.github.com>
2023-08-18 09:53:46 +0900

    app.py

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ from langchain.chains import RetrievalQA
 import os
 import requests
 import pytz
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, 'db')
 
 # Flask 부분
 
@@ -54,7 +52,7 @@ FLASK_SERVER_URL = "http://localhost:5001"  # 예: "http://12345678.ngrok.io"
 # API 키 설정 및 초기화
 os.environ["OPENAI_API_KEY"] = st.secrets["api_key"]
 
-vectordb = Chroma(persist_directory='DB_PATH', embedding_function=OpenAIEmbeddings())
+vectordb = Chroma(persist_directory='https://github.com/janxii/QAlangchain-KICE/tree/main/db', embedding_function=OpenAIEmbeddings())
 retriever = vectordb.as_retriever(search_kwargs={"k": 3})
 qa_chain_global = RetrievalQA.from_chain_type(
     llm=ChatOpenAI(model='gpt-3.5-turbo', temperature=0),
