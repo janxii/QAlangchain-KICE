58e2cdfd4cfb2572d6d285eaf4390cd7450f5546
janxii <133779609+janxii@users.noreply.github.com>
2023-08-19 16:29:30 +0900

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.chains import RetrievalQA
 import os
 import requests
 import pytz
-import pysqlite3
+__import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 # Flask 부분
